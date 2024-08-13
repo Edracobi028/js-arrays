3d8675881c1a0d6c583d24df2bdcfead91345f9c
Edracobi028 <esse_rios_e@hotmail.com>
2024-08-12 21:05:06 -0600

    Some 13/22

---
+++ server/07-some.js
@@ -0,0 +1,93 @@
+const numbers = [1, 2, 3, 4];
+
+/* == Camino Largo == */
+
+let rta1 = false;
+//Encontrar si en el listado de numeros hay un numero par
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element % 2 === 0) {//Si el residuo de dividirlo en dos es cero (validamos si es par)
+        rta1 = true;
+        break; // cortamos la busqueda si encuentra
+    }
+}
+console.log(rta1);
+
+/* == Camino Corto ==   con some()  */
+
+//3. 1er argumento (arrow function) = (item => )
+//4. 2do argumento condicional = {}  (Porque es un objeto)
+let rta2 = numbers.some(item => item % 2 === 0);
+console.log('rta2', rta2);
+
+const orders = [
+    {
+        customerName: "Nicolas",
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 180,
+        delivered: true,
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+    },
+    {
+        customerName: "Nicolas",
+        total: 2322,
+        delivered: false,
+    },
+]
+
+//verificar si hay al menos una orden entregada
+let isDelivered = orders.some(item => item.delivered);
+console.log('Entregadas', isDelivered);
+
+const dates = [
+    {
+        startDate: new Date(2021, 1, 1, 10),
+        endDate: new Date(2021, 1, 1, 11),
+        title: "Cita de trabajo",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita con mi jefe",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 20),
+        endDate: new Date(2021, 1, 1, 21),
+        title: "Cena",
+    },
+];
+
+const newAppointment = { 
+    startDate: new Date(2021, 1, 1, 8), 
+    endDate: new Date(2021, 1, 1, 9, 30), 
+};
+
+const { areIntervalsOverlapping } = require("date-fns");
+
+//crear una funcion para verificar si se traslapa
+//Recibimos una nueva fecha
+const isOverLap = (newDate) => {
+    return dates.some(date => { //Evaluar por cada una de las fechas si se traslapan
+        return areIntervalsOverlapping(
+            {start: date.startDate, end: date.startDate},      //evaluar las fechas que ya estan
+            {start: newDate.startDate, end: newDate.endDate},  //evaluar con la nueva fecha
+        )
+    }); 
+};
+
+console.log( 'isOverlap',isOverLap(newAppointment)  );
+
+
