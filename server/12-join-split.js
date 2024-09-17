e1ed4c278b8d4c2b537cb8e2933bfa3f4303698f
Edracobi028 <esse_rios_e@hotmail.com>
2024-09-16 20:19:52 -0600

    Join 20/28

---
+++ server/12-join-split.js
@@ -0,0 +1,22 @@
+const elements = ["Fire", "Air", "Water"];
+
+/* == solucion larga */
+let phrase = ''; //String vacio para depositar la frase ya unificado
+const separator = '--'; //Lo que separa las palabras
+for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    phrase = phrase + element + separator;
+}
+
+/* == solucion corta con join */
+const phraseTypeJoin = elements.join('--') //Solo se le pasa el caracter que separa
+console.log('for', phrase);
+console.log('phraseTypeJoin', phraseTypeJoin);
+
+
+//Transformar una frase en una URL
+//devuelve en un array cada elemento que resulta de separar por el caracter que le pasamos
+const title = 'Curso de manipulación de arrays';
+const url = title.split(' ').join('-').toLocaleLowerCase();
+console.log(url);
+
